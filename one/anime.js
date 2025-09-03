894613f3512796890e5c82bf4b13a381409c9058
Omkar <omkarjoshiop@gmail.com>
2025-09-03 23:18:23 +0530

    Nodemodules

---
+++ one/anime.js
@@ -0,0 +1,5 @@
+const { AnimeWallpaper, AnimeSource } = require('anime-wallpaper');
+// const wallpaper = new AnimeWallpaper();
+
+// const wallpaper = await wallpaper.random({ resolution: "1920x1080" });
+return console.log(wallpaper)
