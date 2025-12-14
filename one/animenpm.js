38342060eff8ac2a657505c228942b62be1b4999
Omkar <omkarjoshiop@gmail.com>
2025-12-14 17:13:31 +0530

    14Dec serverprac

---
+++ one/animenpm.js
@@ -0,0 +1,17 @@
+// const { AnimeWallpaper } = require('anime-wallpaper');
+// const wallpaper = new AnimeWallpaper();
+
+// (async () => {
+//   try {
+//     const wallpaperr = await wallpaper.random({ resolution: "1920x1080" });
+//     console.log(wallpaperr);
+//   } catch (error) {
+//     console.error('Error fetching wallpaper:', error);
+//   }
+// })();
+
+
+// sqaure root turned js page
+
+const sqr= require ('sqr');
+console.log(sqr(20))
