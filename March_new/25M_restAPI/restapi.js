a5c3e53e6998ada98e687c9bd18552c8322c454e
Omkar <omkarjoshiop@gmail.com>
2026-03-26 21:07:42 +0530

    Designing the api enpoint exercise

---
+++ March_new/25M_restAPI/restapi.js
@@ -0,0 +1,7 @@
+// Gets the list of the courses and their details
+// Gets the details of the course named 1234
+// Creates the course with the provided details
+// Modifies the information of the course with the provided details
+// Gets the average ratings of all the students for the course 1234
+
+// GET api/v1/courses/1234/average-rating
