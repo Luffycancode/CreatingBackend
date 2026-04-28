b543e7c29b2be3391172a8866fbf96c9cee0e024
Omkar <omkarjoshiop@gmail.com>
2026-04-28 19:42:03 +0530

    Filtering data based on Gender

--- March_new/23April_FSModule/server.js
+++ March_new/23April_FSModule/server.js
@@ -1,6 +1,7 @@
 const express=require('express')
-
+const users = require('./Users'); 
 const server= express()
+
 
 server.listen(2000,()=>
 {
@@ -11,5 +12,26 @@ server.listen(2000,()=>
 
 server.get('/',(req,res)=>
 {
-    res.send('On home page')
+    res.json('On home page')
+})
+
+server.get('/gym',(req,res)=>
+{
+res.setHeader("Newheader","My headervalue")
+res.send("On gym homepage")
+})
+
+
+server.get('/api/v1/users',(req,res)=>
+{
+    const data =req.query;
+    console.log(data)
+    const searchedgender=data.gender
+    // console.log(searchedgender)
+
+    const filterdata= users.filter((i)=>i.gender==='Male')
+    console.log(filterdata)
+
+    res.send(filterdata)
+
 })
