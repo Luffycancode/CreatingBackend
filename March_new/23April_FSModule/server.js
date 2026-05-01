3b66349d11d60789a6585c356415807556b40100
Omkar <omkarjoshiop@gmail.com>
2026-05-02 03:59:02 +0530

    Params and query

--- March_new/23April_FSModule/server.js
+++ March_new/23April_FSModule/server.js
@@ -29,9 +29,67 @@ server.get('/api/v1/users',(req,res)=>
     const searchedgender=data.gender
     // console.log(searchedgender)
 
-    const filterdata= users.filter((i)=>i.gender==='Male')
-    console.log(filterdata)
+    // const filterdata= users.filter((i)=>i.gender==='Male')
+    // console.log(filterdata)
 
-    res.send(filterdata)
+    // res.send(filterdata)
+
+
+    const filterdata=users.filter((i)=>
+    {
+        if(i.gender==='Male')
+        {
+            return true
+        }
+            return false
+    })
+
+
+    //also you can do it like this
+    // const payload={
+    // success: true,
+    // data: filterdata,
+    // size: filterdata.length
+    //                 } and pass the payload obj in res.json
+    res.json(
+        {
+            success: true,
+            data:filterdata,
+            length:filterdata.length
+        }
+    )
+
+})
+
+
+//Now this for URL params
+
+
+server.get('/api/v1/users/:userid',(req,res)=>
+{
+    const usrid=Number(req.params.userid) 
+    console.log('This is req params')
+    console.log(usrid)
+
+
+    const idd=users.find((i)=>{
+        return i.id===usrid
+    })
+
+    console.log(typeof(idd))
+    
+    if(!idd)
+    {
+        res.status(400).send("Invalid Request")
+    }
+    else
+    {
+        res.json(idd)
+    }
+
+
+    //Now to send userid from users list Directly
+
+
 
 })
