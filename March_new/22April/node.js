7ef5ed7c514a4de5b285785a58032484a14fee3d
Omkar <omkarjoshiop@gmail.com>
2026-05-24 18:21:24 +0530

    Promises

---
+++ March_new/22April/node.js
@@ -0,0 +1,34 @@
+// const os= require('os')
+
+// // Learning few things about os module
+
+// const platform=os.platform();
+// console.log(platform);
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.totalmem());
+
+
+
+
+// Promise example
+
+
+// let url= fetch('https://jsonplaceholder.typicode.com/posts')
+// .then((response)=>
+// {
+//     return response.json()
+// })
+// .then((data)=>
+// {
+//     console.log(data[0])
+// })
+
+// console.log(a)
+// let a=20
+
+// console.log(data)
+
+
+
+Async and Callback! 
